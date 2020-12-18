bc779016a4be25a2d36442a5b38ae1a6b09bd67f
councit <councit@gmail.com>
2020-12-18 15:15:56 -0800

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
 
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt A Pet</h1>
-      <SearchParams />
+      <header>
+        <Link to="/">Adopt A Pet</Link>
+      </header>
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
 };
---
+++ src/Details.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Details = () => {
+  return (
+    <div>
+      <h1>lol</h1>
+    </div>
+  );
+};
+
+export default Details;
